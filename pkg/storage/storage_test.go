918bc6aea2e490084572b888d3c81c4023243d01
leosocy <leosocy@gmail.com>
2019-05-19 22:07:04 +0800

    test(storage): use suite

--- pkg/storage/storage_test.go
+++ pkg/storage/storage_test.go
@@ -8,96 +8,103 @@ import (
 	"net"
 	"testing"
 
+	"github.com/stretchr/testify/suite"
+
 	"github.com/Leosocy/IntelliProxy/pkg/proxy"
-	"github.com/stretchr/testify/assert"
 )
 
-var testStorages = []Storage{
-	NewInMemoryStorage(),
+type StorageTestSuite struct {
+	suite.Suite
+	storages []Storage
 }
 
-func TestInsert(t *testing.T) {
-	assert := assert.New(t)
-	for _, s := range testStorages {
+func (suite *StorageTestSuite) SetupTest() {
+	suite.storages = []Storage{
+		NewInMemoryStorage(),
+	}
+}
+
+func (suite *StorageTestSuite) TestInsert() {
+	for _, s := range suite.storages {
 		// insert invalid proxy
 		err := s.Insert(nil)
-		assert.Equal(err, ErrProxyInvalid)
+		suite.Equal(err, ErrProxyInvalid)
 		err = s.Insert(&proxy.Proxy{IP: net.ParseIP("1.2.3.4"), Port: 80, Score: 0})
-		assert.Equal(err, ErrProxyInvalid)
+		suite.Equal(err, ErrProxyInvalid)
 		// insert one proxy
 		err = s.Insert(&proxy.Proxy{IP: net.ParseIP("5.6.7.8"), Port: 80, Score: 100})
-		assert.Nil(err)
-		assert.Equal(uint(1), s.Len())
+		suite.Nil(err)
+		suite.Equal(uint(1), s.Len())
 		// insert another proxy
 		err = s.Insert(&proxy.Proxy{IP: net.ParseIP("9.10.11.12"), Port: 80, Score: 100})
-		assert.Equal(uint(2), s.Len())
+		suite.Equal(uint(2), s.Len())
 		// insert proxy with same IP
 		err = s.Insert(&proxy.Proxy{IP: net.ParseIP("9.10.11.12"), Port: 80, Score: 50})
-		assert.Equal(err, ErrProxyDuplicated)
-		assert.Equal(uint(2), s.Len())
+		suite.Equal(err, ErrProxyDuplicated)
+		suite.Equal(uint(2), s.Len())
 	}
 }
 
-func TestSearch(t *testing.T) {
-	assert := assert.New(t)
-	for _, s := range testStorages {
+func (suite *StorageTestSuite) TestSearch() {
+	for _, s := range suite.storages {
 		s.Insert(&proxy.Proxy{IP: net.ParseIP("5.6.7.8"), Port: 80, Score: 100})
 		pxy := s.Search(net.ParseIP("5.6.7.8"))
-		assert.Equal(pxy.IP.String(), "5.6.7.8")
+		suite.Equal(pxy.IP.String(), "5.6.7.8")
 		// not found
 		pxy = s.Search(net.ParseIP("8.8.8.8"))
-		assert.Nil(pxy)
+		suite.Nil(pxy)
 	}
 }
 
-func TestDelete(t *testing.T) {
-	assert := assert.New(t)
-	for _, s := range testStorages {
+func (suite *StorageTestSuite) TestDelete() {
+	for _, s := range suite.storages {
 		p := &proxy.Proxy{IP: net.ParseIP("5.6.7.8"), Port: 80, Score: 100}
 		s.Insert(p)
 		// does not exists
 		err := s.Delete(net.ParseIP("8.8.8.8"))
-		assert.Equal(err, ErrProxyDoesNotExists)
+		suite.Equal(err, ErrProxyDoesNotExists)
 		// normal
 		err = s.Delete(p.IP)
 		searchP := s.Search(p.IP)
-		assert.Nil(err)
-		assert.Nil(searchP)
-		assert.Equal(uint(0), s.Len())
+		suite.Nil(err)
+		suite.Nil(searchP)
+		suite.Equal(uint(0), s.Len())
 	}
 }
 
-func TestBest(t *testing.T) {
-	assert := assert.New(t)
-	for _, s := range testStorages {
+func (suite *StorageTestSuite) TestBest() {
+	for _, s := range suite.storages {
 		// empty
 		bp := s.Best()
-		assert.Nil(bp)
+		suite.Nil(bp)
 		// normal
 		p1 := &proxy.Proxy{IP: net.ParseIP("1.2.3.4"), Port: 80, Score: 50}
 		p2 := &proxy.Proxy{IP: net.ParseIP("5.6.7.8"), Port: 80, Score: 80}
 		s.Insert(p1)
 		s.Insert(p2)
 		bp = s.Best()
-		assert.Equal(p2.IP, bp.IP)
+		suite.Equal(p2.IP, bp.IP)
 	}
 }
 
-func TestUpdate(t *testing.T) {
-	assert := assert.New(t)
-	for _, s := range testStorages {
+func (suite *StorageTestSuite) TestUpdate() {
+	for _, s := range suite.storages {
 		p1 := &proxy.Proxy{IP: net.ParseIP("1.2.3.4"), Port: 80, Score: 50}
 		p2 := &proxy.Proxy{IP: net.ParseIP("5.6.7.8"), Port: 80, Score: 80}
 		s.Insert(p1)
 		s.Insert(p2)
 		// does not exists
 		err := s.Update(&proxy.Proxy{IP: net.ParseIP("6.7.8.9"), Port: 80, Score: 50})
-		assert.Equal(err, ErrProxyDoesNotExists)
+		suite.Equal(err, ErrProxyDoesNotExists)
 		// normal
 		p1.Score = 90
 		err = s.Update(p1)
 		bp := s.Best()
-		assert.Nil(err)
-		assert.Equal(p1.IP, bp.IP)
+		suite.Nil(err)
+		suite.Equal(p1.IP, bp.IP)
 	}
+}
+
+func TestStorageTestSuite(t *testing.T) {
+	suite.Run(t, new(StorageTestSuite))
 }
